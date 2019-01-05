914267d2bb398140056477124c96a2b5c96c0b56
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-05 06:35:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,10 @@
 # Add  code here!
 def prime?(num)
+  if num == 1
+    return true
+  end
   (1..num.abs).each do |i|
-    if (i == num || i == 1)
+    if i == num
       break
     elsif num.abs % i == 0
       return false
